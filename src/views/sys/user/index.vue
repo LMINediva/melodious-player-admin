757d5d011214dd3fdd790f962bf91708564941ec
LMINediva <3037907347@qq.com>
2025-07-04 14:14:37 +0800

    Implement the functions of deleting and batch deleting users.

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -6,8 +6,13 @@
       </el-col>
       <el-button type="primary" :icon="Search" @click="initUserList">搜索</el-button>
       <el-button type="success" :icon="DocumentAdd" @click="handleDialogValue()">新增</el-button>
+      <el-popconfirm title="您确定批量删除这些记录吗？" @confirm="handleDelete(null)">
+        <template #reference>
+          <el-button type="danger" :disabled="delBtnStatus" :icon="Delete">批量删除</el-button>
+        </template>
+      </el-popconfirm>
     </el-row>
-    <el-table :data="tableData" stripe style="width: 100%">
+    <el-table :data="tableData" stripe style="width: 100%" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55"/>
       <el-table-column prop="avatar" label="头像" width="80" align="center">
         <template v-slot="scope">
@@ -36,8 +41,14 @@
           <el-button type="primary" :icon="Tools" @click="handleRoleDialogValue(scope.row.id,scope.row.sysRoleList)">
             分配角色
           </el-button>
-          <el-button v-if="scope.row.username!='java1234'" type="primary" :icon="Edit"
+          <el-button v-if="scope.row.username !== 'java1234'" type="primary" :icon="Edit"
                      @click="handleDialogValue(scope.row.id)"/>
+          <el-popconfirm v-if="scope.row.username !== 'java1234'" title="您确定要删除这条记录吗？"
+                         @confirm="handleDelete(scope.row.id)">
+            <template #reference>
+              <el-button type="danger" :icon="Delete"/>
+            </template>
+          </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
@@ -60,6 +71,7 @@ import {ref} from 'vue';
 import requestUtil, {getServerUrl} from '@/util/request';
 import {Search, Delete, DocumentAdd, Edit, Tools, RefreshRight} from '@element-plus/icons-vue';
 import Dialog from './components/dialog';
+import {ElMessage, ElMessageBox} from 'element-plus';
 
 const tableData = ref([]);
 const total = ref(0);
@@ -72,6 +84,15 @@ const queryForm = ref({
 const dialogVisible = ref(false);
 const dialogTitle = ref("");
 const id = ref(-1);
+const delBtnStatus = ref(true);
+const multipleSelection = ref([]);
+
+const handleSelectionChange = (selection) => {
+  console.log("勾选了");
+  console.log(selection);
+  multipleSelection.value = selection;
+  delBtnStatus.value = selection.length === 0;
+};
 
 const initUserList = async () => {
   const res = await requestUtil.post("sys/user/list", queryForm.value);
@@ -101,6 +122,30 @@ const handleDialogValue = (userId) => {
     dialogTitle.value = "用户添加";
   }
   dialogVisible.value = true;
+};
+
+const handleDelete = async (id) => {
+  var ids = [];
+  if (id) {
+    ids.push(id);
+  } else {
+    multipleSelection.value.forEach(row => {
+      ids.push(row.id);
+    });
+  }
+  const res = await requestUtil.post("sys/user/delete", ids);
+  if (res.data.code === 200) {
+    ElMessage({
+      type: 'success',
+      message: '执行成功!'
+    });
+    initUserList();
+  } else {
+    ElMessage({
+      type: 'error',
+      message: res.data.msg,
+    });
+  }
 };
 </script>
 
